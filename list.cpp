bbae2f8cd43186d7b380c354ec16708d5e6e209c
buzeqq <miloszchoj@gmail.com>
2021-03-11 19:52:13 +0100

    Initial commit

---
+++ list.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Milosz Chojnacki on 11.03.2021.
+//
+#include "list.h"
+
+template<class T>
+List<T>::List() {
+    std::cout << "it works!" << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "list.h"
+
+using namespace std;
+
+int main() {
+
+    List<int> list;
+
+    return 0;
+}
