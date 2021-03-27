1d6bc17f33e641837e545dcbc4d14539f692b7f5
buzeqq <miloszchoj@gmail.com>
2021-03-27 16:40:56 +0100

    final version (10/10)

--- main.cpp
+++ main.cpp
@@ -11,11 +11,6 @@ struct Program {
     }
 };
 
-/*ostream& operator<<(ostream& ostream, Program& pro) {
-    ostream << pro.ID;
-    return ostream;
-}*/
-
 int CompareSizes(const Program& p1, const Program& p2) {
     if (p1.size < p2.size) return -1;
     if (p1.size == p2.size) return 0;
@@ -65,44 +60,38 @@ void Sort(Program array[], int left, int right, F compareFunction) {
 }
 
 void Division(Program array[], int size, int memory) {
-    int sum1 = 0, sum2 = 0, i = 0, counter = 0;
-    while (true) {
-        if (i >= size) break;
-        if (sum1 + array[i].size < memory) {
-            sum1 += array[i++].size;
-            counter++;
-        }
-        else break;
+    int sum = 0, i = 0;
+    while (i <= size - 1 && sum + array[i].size <= memory) {
+        sum += array[i].size;
+        i++;
     }
-    auto* pendrive1 = new Program[counter];
-    int tmp = counter;
-    for (int j = 0; j < tmp; j++) {
+    int sizePen1 = i;
+    auto* pendrive1 = new Program[sizePen1];
+    for (int j = 0; j < sizePen1; j++) {
         pendrive1[j] = array[j];
     }
-    Sort(pendrive1, 0, counter - 1, CompareIDs);
 
-    while (true) {
-        if (i >= size) break;
-        if (sum2 + array[i].size < memory) {
-            sum2 += array[i++].size;
-            counter++;
-        }
-        else break;
+    sum = 0;
+    while (i <= size - 1 && sum + array[i].size <= memory) {
+        sum += array[i].size;
+        i++;
     }
-    auto* pendrive2 = new Program[counter - tmp];
-    for (int j = 0; j < counter - tmp; j++) {
-        pendrive2[j] = array[tmp + j];
+    int sizePen2 = i - sizePen1;
+    auto* pendrive2 = new Program[sizePen2];
+    for (int j = 0; j < sizePen2; j++) {
+        pendrive2[j] = array[sizePen1 + j];
     }
-    Sort(pendrive2, 0, counter - tmp - 1, CompareIDs);
 
-    printf("%d\n1: ", counter);
-    for (i = 0; i < tmp; i++) {
-        // cout << pendrive1[i] << " ";
-        printf("%d ", pendrive1[i].ID);
+    Sort(pendrive1, 0, sizePen1 - 1, CompareIDs);
+    Sort(pendrive2, 0, sizePen2 - 1, CompareIDs);
+
+    printf("%d\n1: ", i);
+    for (int j = 0; j < sizePen1; j++) {
+        printf("%d ", pendrive1[j].ID);
     }
     printf("\n2: ");
-    for (i = 0; i < counter - tmp; i++) {
-        printf("%d ", pendrive2[i].ID);
+    for (int j = 0; j < sizePen2; j++) {
+        printf("%d ", pendrive2[j].ID);
     }
 
     delete[] pendrive1;
@@ -112,12 +101,12 @@ void Division(Program array[], int size, int memory) {
 int main() {
 
     int memory = 0, programs = 0;
-    scanf("%d %d", &programs, &memory);
+    if (scanf("%d %d", &programs, &memory) != 2) return -1;
     auto* array = new Program[programs];
 
     for (int i = 0; i < programs; i++) {
         Program tmp;
-        scanf("%d %d", &(tmp.ID), &(tmp.size));
+        if (scanf("%d %d", &(tmp.ID), &(tmp.size)) != 2) return -1;
         array[i] = tmp;
     }
 
